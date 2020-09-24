257ef956188520de4f70fd78e2ca42d1745638b8
Dwakar <anishdiwakar99@gmail.com>
2020-09-24 17:42:13 +0545

    Colab

---
+++ movie/moviecolab.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from math import sqrt
+import numpy as np
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+
+ratings = pd.read_csv("ratings.csv")
+movies = pd.read_csv("movies.csv")
+
+ratings = pd.merge(movies,ratings).drop(['genres','timestamp'],axis=1)
+
+user_ratings = ratings.pivot_table(index=['userId'],columns=['title'],values='rating')#change matrix rows and columns
+
+#remove movies which have less than 10 users who rated it
+user_ratings = user_ratings.dropna(thresh=10,axis=1)
+user_ratings = user_ratings.fillna(0)
+
+item_similarity_df = user_ratings.corr(method="pearson")
+
+def get_similar_movies(movie_name,user_rating):
+	similar_score = item_similarity_df[movie_name]*(user_rating-2.5)
+	similar_score = similar_score.sort_values(ascending=False)
+	return similar_score
+	
+romantic_lover = [("Dracula: Dead and Loving It (1995)",5),("Money Train (1995)",5),("Aliens (1986)",1),("2001: A Space Odyssey (1968)",0)]
+similar_movies = pd.DataFrame()
+for products,ratings in romantic_lover:
+    similar_movies = similar_movies.append(get_similar_movies(products,ratings),ignore_index = True)
+
+print("Recommended movies according to user input are as follows:")
+print(similar_movies.sum().sort_values(ascending=False).head(20))
---
+++ restaurant/restcolab.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+
+ratings = pd.read_csv('rating_final.csv')
+cuisine= pd.read_csv('usercuisine.csv')
+
+ratings = pd.merge(cuisine,ratings).drop(['placeID','food_rating','service_rating'],axis=1)
+user_ratings = ratings.pivot_table(values='rating',index='userID',columns='Rcuisine')#change matrix rows and columns
+user_ratings = user_ratings.dropna(thresh=10,axis=1)
+user_ratings = user_ratings.fillna(0)
+
+item_similarity_df = user_ratings.corr(method="pearson")
+print(item_similarity_df)
+# def get_similar_cuisine(cuisine_name,user_rating):
+# 	similar_score = item_similarity_df[cuisine_name]*(user_rating-2.5)
+# 	similar_score = similar_score.sort_values(ascending=False)
+# 	return similar_score
+
+# food_loved = [("American",5),("Mexican",1)]
+# similar_cuisine = pd.DataFrame()
+# for movie,rating in food_loved:
+#     similar_cuisine = similar_cuisine.append(get_similar_cuisine(movie,rating),ignore_index = True)
+
+# print("Recommended cuisine according to user input are as follows:")
+# print(similar_cuisine.sum().sort_values(ascending=False).head(20))
